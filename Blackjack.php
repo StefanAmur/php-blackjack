e9aab80c42982076f5b4b52892dbe59d42cc6163
StefanAmur <stefan.amur22@gmail.com>
2021-10-28 10:56:18 +0200

    Added the required files, hopefully

---
+++ Blackjack.php
@@ -0,0 +1,23 @@
+<?php
+
+class Blackjack {
+    // properties
+    private $player;
+
+    private $dealer;
+
+    private $deck;
+
+    // methods
+    public function getPlayer() {
+        return $this->player;
+    }
+
+    public function getDealer() {
+        return $this->dealer;
+    }
+
+    public function getDeck() {
+        return $this->deck;
+    }
+}
---
+++ Player.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+class Player {
+    // properties
+    private $cards = [];
+    private $lost = false;
+
+    // methods
+    public function hit() {
+    }
+
+    public function surrender() {
+    }
+
+    public function getScore() {
+    }
+
+    public function hasLost() {
+    }
+}
